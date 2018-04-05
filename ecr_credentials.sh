04fb9491bc268b0892c6816039eefa21557456a9
Roland Seidel <register@interpolar.ch>
2018-04-05 23:16:57 +0200

    integrate signing-feature

--- ecr_credentials.sh
+++ ecr_credentials.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# make script executable: git update-index --add --chmod=+x ecr_credentials.sh
+
 mkdir -p ~/.aws
 
 cat > ~/.aws/credentials << EOL
@@ -7,4 +9,18 @@ cat > ~/.aws/credentials << EOL
 aws_default_region = eu-west-1
 aws_access_key_id = ${aws_user}
 aws_secret_access_key = ${aws_accesskey}
+EOL
+
+cat > client/mobile-ticket-queue/build.json << EOL
+{
+  "android": {
+      "release": {
+          "keystore": "~/requisites/mobile-ticket-queue.jks",
+          "storePassword": "${keystorePassword}",
+          "alias": "mobile-ticket-queue",
+          "password" : "${keystorePassword}",
+          "keystoreType": ""
+      }
+  }
+}
 EOL
